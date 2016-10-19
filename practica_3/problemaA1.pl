dfe461fa81d89044067be86e9131f7feb38ca357
Reynau <xavi.lopez.reynau@gmail.com>
2016-10-19 18:58:28 +0200

    Fix bug. Works!

--- practica_3/problemaA1.pl
+++ practica_3/problemaA1.pl
@@ -6,18 +6,25 @@ camino( EstadoActual, EstadoFinal, CaminoHastaAhora, CaminoTotal ):-
   camino( EstSiguiente, EstadoFinal, [EstSiguiente|CaminoHastaAhora], CaminoTotal ).
 
 % LLenamos los cubos
-unPaso([0,_],[5,_]).
-unPaso([_,0],[_,8]).
+unPaso([_,C2],[5,C2]).
+unPaso([C1,_],[C1,8]).
 % Vaciamos los cubos
-unPaso([5,_],[0,_]).
-unPaso([_,8],[_,0]).
+unPaso([_,C2],[0,C2]).
+unPaso([C1,_],[C1,0]).
 % Verter contenido
-unPaso([_,8],[5,0]). % El de 8 en el de 5
-unPaso([5,0],[0,5]). % El de 5 en un cubo8 vacio
-unPaso([5,8],[0,8]). % El de 5 en un cubo8 lleno
+unPaso([C1,C2],[C1F,C2F]) :-
+  C2F is min(8, C2 + C1),
+  C1F is C1 - (C2F - C2).
+unPaso([C1,C2],[C1F,C2F]) :-
+  C1F is min(5, C1 + C2),
+  C2F is C2 - (C1F - C1).
+
 
 solucionOptima:-
   nat(N),                         % Buscamos solución de "coste" 0; si no, de 1, etc.
   camino([0,0],[0,4],[[0,0]],C),  % En "hacer aguas": -un estado es [cubo5, cubo8], y
   length(C,N),                    % -el coste es la longitud de C.
   write(C).
+
+nat(0).
+nat(N):- nat(N1), N is N1 + 1.
