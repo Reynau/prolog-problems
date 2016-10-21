39f4551de7d8699c1c33985a899664ac7e34d81d
Reynau <xavi.lopez.reynau@gmail.com>
2016-10-21 16:51:19 +0200

    Optimize problemaA2

--- practica_3/ProblemaA/problemaA2.pl
+++ practica_3/ProblemaA/problemaA2.pl
@@ -1,34 +1,35 @@
+% https://es.wikipedia.org/wiki/Problema_del_caballo
+pasos(4).
+tamano(8).
 
-pasos(15).
-
-camino(E,E, C,C,_).
-camino(EstadoActual, EstadoFinal, CaminoHastaAhora, CaminoTotal,Limite):-
-    unPaso(EstadoActual, EstSiguiente, Limite),
-    \+member(EstSiguiente, CaminoHastaAhora),
-    pasos(P),
-    length(CaminoHastaAhora,L),
-    L =< P,
-    camino(EstSiguiente, EstadoFinal, [EstSiguiente|CaminoHastaAhora], CaminoTotal,Limite).
+camino( E,E, C,C, 0).
+camino( EstadoActual, EstadoFinal, CaminoHastaAhora, CaminoTotal, P ):-
+  P > 0,
+  unPaso( EstadoActual, EstSiguiente ),
+  \+member( EstSiguiente, CaminoHastaAhora ),
+  P1 is P-1,
+  camino( EstSiguiente, EstadoFinal, [EstSiguiente|CaminoHastaAhora], CaminoTotal, P1).
 
 % 2 Up 1 Right
-unPaso([F1,C1],[FF,CF],N) :- F1 + 1 < N, C1 + 2 < N, FF is F1 + 1, CF is C1 + 2.
+unPaso([F1,C1],[FF,CF]) :- tamano(N), F1 + 1 < N, C1 + 2 < N, FF is F1 + 1, CF is C1 + 2.
 % 2 Up 1 Left
-unPaso([F1,C1],[FF,CF],N) :- F1 - 1 >= 0, C1 + 2 < N, FF is F1 - 1, CF is C1 + 2.
+unPaso([F1,C1],[FF,CF]) :- tamano(N), F1 - 1 >= 0, C1 + 2 < N, FF is F1 - 1, CF is C1 + 2.
 % 2 Down 1 Right
-unPaso([F1,C1],[FF,CF],N) :- F1 + 1 < N, C1 - 2 >= 0, FF is F1 + 1, CF is C1 - 2.
+unPaso([F1,C1],[FF,CF]) :- tamano(N), F1 + 1 < N, C1 - 2 >= 0, FF is F1 + 1, CF is C1 - 2.
 % 2 Down 1 Left
-unPaso([F1,C1],[FF,CF],N) :- F1 - 1 >= 0, C1 - 2 >= 0, FF is F1 - 1, CF is C1 - 2.
+unPaso([F1,C1],[FF,CF]) :- tamano(N), F1 - 1 >= 0, C1 - 2 >= 0, FF is F1 - 1, CF is C1 - 2.
 % 2 Right 1 Up
-unPaso([F1,C1],[FF,CF],N) :- F1 + 2 < N, C1 + 1 < N, FF is F1 + 2, CF is C1 + 1.
+unPaso([F1,C1],[FF,CF]) :- tamano(N), F1 + 2 < N, C1 + 1 < N, FF is F1 + 2, CF is C1 + 1.
 % 2 Right 1 Down
-unPaso([F1,C1],[FF,CF],N) :- F1 + 2 < N, C1 - 1 >= 0, FF is F1 + 2, CF is C1 - 1.
+unPaso([F1,C1],[FF,CF]) :- tamano(N), F1 + 2 < N, C1 - 1 >= 0, FF is F1 + 2, CF is C1 - 1.
 % 2 Left 1 Up
-unPaso([F1,C1],[FF,CF],N) :- F1 - 2 >= 0, C1 + 1 < N, FF is F1 - 2, CF is C1 + 1.
+unPaso([F1,C1],[FF,CF]) :- tamano(N), F1 - 2 >= 0, C1 + 1 < N, FF is F1 - 2, CF is C1 + 1.
 % 2 Left 1 Down
-unPaso([F1,C1],[FF,CF],N) :- F1 - 2 >= 0, C1 - 1 >= 0, FF is F1 - 2, CF is C1 - 1.
+unPaso([F1,C1],[FF,CF]) :- tamano(N), F1 - 2 >= 0, C1 - 1 >= 0, FF is F1 - 2, CF is C1 - 1.
 
 solucionOptima:-
-  camino([1,1],  [0,0],  [[1,1]],  C, 6),
   pasos(P),
-  length(C,P),
-  write(C).
+  camino([0,0],[0,4],[[0,0]],C,P),
+  !,
+  write(C),
+  nl.
